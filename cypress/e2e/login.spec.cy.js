ade67a4ad30d834b5a1eb9774d1a55e84b9ce715
C1322309 Tiago Lira Batista <c1322309@interno.bb.com.br>
2024-10-14 15:21:49 -0300

    [cypress.config] basUrl

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -12,10 +12,9 @@ describe('Orange HRM Test', () => {
   }
 
 
-
   it('Login realizado com sucesso', () => {
 
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
